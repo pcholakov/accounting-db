900001024681590f11ff794922a8c6800a53b402
Pavel Tcholakov <pcholakov@gmail.com>
2023-09-26 10:12:52 +0200

    Add warmup phase to test harness

--- load-test/driver.ts
+++ load-test/driver.ts
@@ -1,4 +1,4 @@
-import { createHistogram, Histogram, PerformanceObserver, performance, RecordableHistogram } from "perf_hooks";
+import { Histogram, RecordableHistogram, createHistogram, performance } from "perf_hooks";
 
 export interface Test {
   setup(): Promise<void>;
@@ -10,16 +10,21 @@ export class LoadTestDriver {
   private concurrency: number;
   private arrivalRate: number;
   private test: Test;
-  private duration: number;
-  private _histogram: RecordableHistogram;
+  private overallDurationMs: number;
+  private warmupDurationMs: number;
+  private _durationMicrosHistogram: RecordableHistogram;
   private _requestCount: number = 0;
+  private _errorCount: number = 0;
+  private benchmarkDurationMs: number;
 
-  constructor(concurrency: number, arrivalRate: number, duration: number, test: Test) {
+  constructor(concurrency: number, arrivalRate: number, durationSeconds: number, test: Test) {
     this.concurrency = concurrency;
     this.arrivalRate = arrivalRate;
-    this.duration = duration;
+    this.overallDurationMs = durationSeconds * 1_000;
+    this.warmupDurationMs = Math.min(this.overallDurationMs / 10, 10_000); // 10% of duration or 10s, whichever is smaller
+    this.benchmarkDurationMs = this.overallDurationMs - this.warmupDurationMs;
     this.test = test;
-    this._histogram = createHistogram();
+    this._durationMicrosHistogram = createHistogram();
   }
 
   async run(): Promise<void> {
@@ -27,27 +32,49 @@ export class LoadTestDriver {
 
     const intervalMs = (1000 * this.concurrency) / this.arrivalRate;
     const startTime = performance.now();
-    const endTime = startTime + this.duration * 1000;
+    const warmupEndime = startTime + this.warmupDurationMs;
+    const endTime = startTime + this.overallDurationMs;
 
     const workers: Array<Promise<void>> = [];
 
     const concurrentWorkerLoop = async (id: string) => {
       await sleep(Math.random() * intervalMs); // startup jitter
 
-      do {
+      // Warmup - run work loop without measuring
+      while (performance.now() < warmupEndime) {
         const iterationStart = performance.now();
-        await this.test.request();
-        const iterationEnd = performance.now();
-
-        const iterationDuration = iterationEnd - iterationStart;
-        this._requestCount++;
-        const durationInt = Math.floor(iterationDuration);
-        if (durationInt > 0) {
-          this._histogram.record(durationInt);
+        try {
+          await this.test.request();
+        } catch (error) {
+          // ignore errors during warmup
         }
-
+        const iterationEnd = performance.now();
+        const iterationDuration = iterationEnd - iterationStart;
         if (iterationDuration < intervalMs) {
           await sleep(intervalMs - iterationDuration);
+        }
+      }
+
+      // Measurement loop
+      do {
+        const iterationStart = performance.now();
+        try {
+          await this.test.request();
+        } catch (error) {
+          this._errorCount++;
+        }
+        const iterationEnd = performance.now();
+
+        const iterationDurationMicros = (iterationEnd - iterationStart) * 1_000;
+        this._requestCount++;
+        const durationInt = Math.floor(iterationDurationMicros);
+        if (durationInt > 0) {
+          // TODO: surface zero durations to the user
+          this._durationMicrosHistogram.record(durationInt);
+        }
+
+        if (iterationDurationMicros < intervalMs) {
+          await sleep(intervalMs - iterationDurationMicros);
         }
       } while (performance.now() < endTime);
     };
@@ -67,20 +94,25 @@ export class LoadTestDriver {
       configuration: {
         concurrency: this.concurrency,
         targetArrivalRate: this.arrivalRate,
-        duration: this.duration,
+        duration: this.overallDurationMs,
+        warmup: this.warmupDurationMs,
       },
-      count: this.requestCount(),
-      throughput: this.requestCount() / this.duration,
-      arrivalRateRatio: this.requestCount() / this.duration / this.arrivalRate,
-      avg: this.histogram().mean,
-      min: this.histogram().min,
-      max: this.histogram().max,
-      p50: this.histogram().percentile(50),
-      p75: this.histogram().percentile(75),
-      p90: this.histogram().percentile(90),
-      p95: this.histogram().percentile(95),
-      p99: this.histogram().percentile(99),
-      p999: this.histogram().percentile(99.9),
+      requestCount: this.requestCount(),
+      errorCount: this.errorCount(),
+      errorRate: this.errorCount() / this.requestCount(),
+      throughput: (this.requestCount() * 1_000) / this.benchmarkDurationMs,
+      arrivalRateRatio: (this.requestCount() * 1_000) / this.benchmarkDurationMs / this.arrivalRate,
+      durationsMillis: {
+        avg: this.histogram().mean / 1_000,
+        p0: this.histogram().min / 1_000,
+        p50: this.histogram().percentile(50) / 1_000,
+        p75: this.histogram().percentile(75) / 1_000,
+        p90: this.histogram().percentile(90) / 1_000,
+        p95: this.histogram().percentile(95) / 1_000,
+        p99: this.histogram().percentile(99) / 1_000,
+        p99_9: this.histogram().percentile(99.9) / 1_000,
+        p100: this.histogram().max / 1_000,
+      },
     });
   }
 
@@ -88,8 +120,12 @@ export class LoadTestDriver {
     return this._requestCount;
   }
 
+  errorCount() {
+    return this._errorCount;
+  }
+
   histogram(): Histogram {
-    return this._histogram;
+    return this._durationMicrosHistogram;
   }
 }
 

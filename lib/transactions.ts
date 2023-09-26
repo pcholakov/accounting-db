614219d071347da2b9b33f40bc266af20688e567
Pavel Tcholakov <pcholakov@gmail.com>
2023-09-26 17:19:33 +0200

    Make batch transfer writing work correctly

--- lib/transactions.ts
+++ lib/transactions.ts
@@ -1,6 +1,7 @@
 import * as ddc from "@aws-sdk/lib-dynamodb";
 import { randomUUID } from "crypto";
 import { TransactWriteCommandInput } from "@aws-sdk/lib-dynamodb";
+import assert from "assert";
 
 export interface Transfer {
   id: IdType;
@@ -65,6 +66,22 @@ export async function createAccount(
   );
 }
 
+export async function getAccount(
+  client: ddc.DynamoDBDocumentClient,
+  tableName: string,
+  accountId: AccountId,
+): Promise<Account | undefined> {
+  const result = await client.send(
+    new ddc.GetCommand({ TableName: tableName, Key: { pk: `account#${accountId}`, sk: `account#${accountId}` } }),
+  );
+  if (!result.Item) {
+    return undefined;
+  }
+
+  const { pk, sk, ...account } = result.Item;
+  return { id: accountId, ...account } as Account;
+}
+
 export async function createTransfer(
   documentClient: ddc.DynamoDBDocumentClient,
   tableName: string,
@@ -79,51 +96,77 @@ export async function createTransfersBatch(
   batch: Transfer[],
 ): Promise<TransferResult> {
   if (batch.length > 33) {
-    throw new Error("Assertion error: Bbatch size too large");
+    throw new Error("Assertion error: Batch size too large");
   }
 
-  const items: TransactWriteCommandInput["TransactItems"] = [];
+  type TransactItems = TransactWriteCommandInput["TransactItems"];
+  type ItemType = NonNullable<TransactItems>[number];
+  const items: TransactItems = [];
+  const pendingUpdates: Map<AccountId, ItemType> = new Map();
 
   batch.forEach((transfer) => {
-    items.push(
-      {
-        Put: {
-          TableName: tableName,
-          Item: {
-            pk: `transfer#${transfer.id}`,
-            sk: `transfer#${transfer.id}`,
-            ...transfer,
-          },
-          ConditionExpression: "attribute_not_exists(pk)",
+    items.push({
+      Put: {
+        TableName: tableName,
+        Item: {
+          pk: `transfer#${transfer.id}`,
+          sk: `transfer#${transfer.id}`,
+          ...transfer,
         },
+        ConditionExpression: "attribute_not_exists(pk)",
       },
-      {
+    });
+
+    const debitAccountPendingTx = pendingUpdates.get(transfer.debit_account_id);
+    const creditAccountPendingTx = pendingUpdates.get(transfer.credit_account_id);
+
+    if (debitAccountPendingTx) {
+      const debit_amount = debitAccountPendingTx?.Update?.ExpressionAttributeValues?.[":debit_amount"];
+      assert(debit_amount !== undefined);
+      debitAccountPendingTx!.Update!.ExpressionAttributeValues![":debit_amount"] = debit_amount + transfer.amount;
+    } else {
+      const updateDebitBalance = {
         Update: {
           TableName: tableName,
           Key: {
             pk: `account#${transfer.debit_account_id}`,
             sk: `account#${transfer.debit_account_id}`,
           },
-          UpdateExpression: "SET debits_posted = debits_posted + :amount",
+          UpdateExpression:
+            "SET debits_posted = debits_posted + :debit_amount, credits_posted = credits_posted + :credit_amount",
           ExpressionAttributeValues: {
-            ":amount": transfer.amount,
+            ":debit_amount": transfer.amount,
+            ":credit_amount": 0,
           },
         },
-      },
-      {
+      };
+      items.push(updateDebitBalance);
+      pendingUpdates.set(transfer.debit_account_id, updateDebitBalance);
+    }
+
+    if (creditAccountPendingTx) {
+      const credit_amount = creditAccountPendingTx?.Update?.ExpressionAttributeValues?.[":credit_amount"];
+      assert(credit_amount !== undefined);
+      creditAccountPendingTx!.Update!.ExpressionAttributeValues![":credit_amount"] = credit_amount + transfer.amount;
+    } else {
+      const updateCreditBalance = {
         Update: {
           TableName: tableName,
           Key: {
             pk: `account#${transfer.credit_account_id}`,
             sk: `account#${transfer.credit_account_id}`,
           },
-          UpdateExpression: "SET credits_posted = credits_posted + :amount",
+          UpdateExpression:
+            "SET debits_posted = debits_posted + :debit_amount, credits_posted = credits_posted + :credit_amount",
           ExpressionAttributeValues: {
-            ":amount": transfer.amount,
+            ":debit_amount": 0,
+            ":credit_amount": transfer.amount,
           },
         },
-      },
-    );
+      };
+      items.push(updateCreditBalance);
+      pendingUpdates.set(transfer.credit_account_id, updateCreditBalance);
+    }
   });
 
   await documentClient.send(
--- test/transactions.test.ts
+++ test/transactions.test.ts
@@ -1,6 +1,10 @@
 import * as dynamodb from "@aws-sdk/client-dynamodb";
 import * as ddc from "@aws-sdk/lib-dynamodb";
-import { createAccount, createTransfer } from "../lib/transactions.js";
+import { Transfer, TransferResult, createAccount, createTransfersBatch, getAccount } from "../lib/transactions.js";
+
+const util = require("util");
+
+util.inspect.defaultOptions.depth = null;
 
 const dynamoDbClient = new dynamodb.DynamoDBClient({
   region: "localhost",
@@ -77,43 +81,83 @@ describe("transactions", () => {
       ]);
     });
 
-    test("create transfers", async () => {
-      await createTransfer(documentClient, TABLE_NAME, {
-        id: "1",
-        ledger: 700,
-        amount: 10,
-        debit_account_id: 1,
-        credit_account_id: 2,
-        code: 0,
-        flags: 0,
-        pending_id: undefined,
-        timeout: 0,
-        timestamp: Date.now(),
-      });
-      await createTransfer(documentClient, TABLE_NAME, {
-        id: "2",
-        ledger: 700,
-        amount: 10,
-        debit_account_id: 2,
-        credit_account_id: 1,
-        code: 0,
-        flags: 0,
-        pending_id: undefined,
-        timeout: 0,
-        timestamp: Date.now(),
-      });
-      await createTransfer(documentClient, TABLE_NAME, {
-        id: "3",
-        ledger: 700,
-        amount: 10,
-        debit_account_id: 1,
-        credit_account_id: 3,
-        code: 0,
-        flags: 0,
-        pending_id: undefined,
-        timeout: 0,
-        timestamp: Date.now(),
-      });
+    test("create transfers batch", async () => {
+      const transfers: Transfer[] = [
+        {
+          id: "4",
+          ledger: 700,
+          amount: 10,
+          debit_account_id: 1,
+          credit_account_id: 2,
+          code: 0,
+          flags: 0,
+          pending_id: undefined,
+          timeout: 0,
+          timestamp: Date.now(),
+        },
+        {
+          id: "5",
+          ledger: 700,
+          amount: 20,
+          debit_account_id: 2,
+          credit_account_id: 1,
+          code: 0,
+          flags: 0,
+          pending_id: undefined,
+          timeout: 0,
+          timestamp: Date.now(),
+        },
+        {
+          id: "6",
+          ledger: 700,
+          amount: 30,
+          debit_account_id: 1,
+          credit_account_id: 3,
+          code: 0,
+          flags: 0,
+          pending_id: undefined,
+          timeout: 0,
+          timestamp: Date.now(),
+        },
+      ];
+
+      const result = await createTransfersBatch(documentClient, TABLE_NAME, transfers);
+      expect(result).toEqual(TransferResult.OK);
+
+      const accounts = await Promise.all([
+        getAccount(documentClient, TABLE_NAME, 1),
+        getAccount(documentClient, TABLE_NAME, 2),
+        getAccount(documentClient, TABLE_NAME, 3),
+      ]);
+
+      expect(accounts).toEqual([
+        {
+          id: 1,
+          ledger: 700,
+          debits_pending: 0,
+          debits_posted: 40,
+          credits_pending: 0,
+          credits_posted: 20,
+        },
+        {
+          id: 2,
+          ledger: 700,
+          debits_pending: 0,
+          debits_posted: 20,
+          credits_pending: 0,
+          credits_posted: 10,
+        },
+        {
+          id: 3,
+          ledger: 700,
+          debits_pending: 0,
+          debits_posted: 0,
+          credits_pending: 0,
+          credits_posted: 30,
+        },
+      ]);
+
+      // TODO: validate the transfers table
     });
   });
 });

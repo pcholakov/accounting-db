40384405da5f75587b2d64f93f06838ebd1802f4
Pavel Tcholakov <pcholakov@gmail.com>
2023-10-09 14:24:44 +0200

    CDK stack cleanup - remove unused fn and queue

--- lib/cdk/accounting-db-stack.ts
+++ lib/cdk/accounting-db-stack.ts
@@ -2,7 +2,6 @@ import * as cdk from "aws-cdk-lib";
 import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as lambda_node from "aws-cdk-lib/aws-lambda-nodejs";
-import * as sqs from "aws-cdk-lib/aws-sqs";
 import { Construct } from "constructs";
 import * as path from "path";
 import { fileURLToPath } from "url";
@@ -13,8 +12,6 @@ const __dirname = path.dirname(__filename);
 export class AccountingDbStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    const queue = new sqs.Queue(this, "TxWrites", {});
 
     /// On-demand gives us adaptive capacity that scales down to zero when not
     /// in use; you should get a baseline of 10,000 WCU which allows up to ~3333
@@ -49,28 +46,6 @@ export class AccountingDbStack extends cdk.Stack {
 
       contributorInsightsEnabled: true,
       removalPolicy: cdk.RemovalPolicy.DESTROY, // Don't do this with real data you care about!
-    });
-
-    const writerFunction = new lambda_node.NodejsFunction(this, "Writer", {
-      memorySize: 1024,
-      timeout: cdk.Duration.seconds(5),
-      runtime: lambda.Runtime.NODEJS_18_X,
-      handler: "handler",
-      entry: path.join(__dirname, "../lambda/writer.ts"),
-      environment: {
-        QUEUE_NAME: queue.queueName,
-        TABLE_NAME: table.tableName,
-      },
-    });
-    table.grantWriteData(writerFunction);
-    queue.grantConsumeMessages(writerFunction);
-
-    // Crate event source mapping for queue
-    new lambda.EventSourceMapping(this, "QueueEventSourceMapping", {
-      eventSourceArn: queue.queueArn,
-      batchSize: 1,
-      enabled: true,
-      target: writerFunction,
     });
 
     const benchmarkTransfers = new lambda_node.NodejsFunction(this, "BenchmarkTransfers", {

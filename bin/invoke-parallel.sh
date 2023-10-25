18ac1b9f70329edbf89e8a49958ebd73fddd0231
Pavel Tcholakov <pcholakov@gmail.com>
2023-10-25 09:15:59 +0200

    Fixup parallel benchmark invocation script

--- bin/invoke-parallel.sh
+++ bin/invoke-parallel.sh
@@ -3,8 +3,26 @@ set -eu -o pipefail
 
 script_dir=$(dirname "$0")
 
-for _ in {1..10}; do
+if [[ $# -ne 1 ]]; then
+    echo "Usage: $0 <workers>"
+    exit 1
+fi
+
+workers=$1
+
+if ! [[ $workers =~ ^[1-9][0-9]*$ ]]; then
+    echo "Error: <workers> must be an integer"
+    exit 1
+fi
+
+date
+echo "Running $workers tests in parallel..."
+for _ in $(seq 1 $workers); do
+    echo "Starting worker..."
     ${script_dir}/invoke-benchmark.sh &
 done
 
+date
+echo "Waiting for workers to finish..."
 wait
+date

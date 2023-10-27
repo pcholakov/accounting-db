b6643077c1ddbab7a8fca89bb0f670ef8c2efe61
Pavel Tcholakov <pcholakov@gmail.com>
2023-10-27 17:24:37 +0200

    Tweak scheduling logic and throughput calculations

--- lib/load-test-runner.ts
+++ lib/load-test-runner.ts
@@ -86,7 +86,12 @@ export class LoadTestDriver {
 
     const concurrentWorkerLoop = async (id: string) => {
       // Jitter startup and perform the warmup iterations without measuring
-      await sleep(Math.random() * this.workerCycleTimeMs);
+      await sleep(
+        Math.round(
+          Math.random() *
+            (this.warmupDurationMs > 2 * this.workerCycleTimeMs ? this.warmupDurationMs / 2 : this.workerCycleTimeMs),
+        ),
+      );
       await this.doWarmup(warmupEndTime, this.workerCycleTimeMs);
 
       // Measurement loop
@@ -96,7 +101,6 @@ export class LoadTestDriver {
         const requestStart = performance.now();
         try {
           await this.test.performIteration();
-          this.requestCount += this.requestsPerIteration;
         } catch (err) {
           if (this.errorIterations % 1000 == 0) {
             console.error(`Worker ${id} failed iteration ${this.iterationCount}`, err);
@@ -104,52 +108,64 @@ export class LoadTestDriver {
           error = true;
           this.errorIterations += 1;
         }
+
         const iterationCompleted = performance.now();
-        this.iterationCount += 1;
+        if (iterationCompleted > endTime) {
+          break;
+        }
 
         const iterationDurationMillis = iterationCompleted - iterationStart;
         const serviceTimeMillis = iterationCompleted - requestStart;
         this.recordDuration(this.requestLatencyMicros, Math.round(iterationDurationMillis * 1000));
         this.recordDuration(this.serviceTimeMicros, Math.round(serviceTimeMillis * 1000));
+        this.iterationCount += 1;
+        this.requestCount += this.requestsPerIteration;
         this.workerRunTime += serviceTimeMillis;
 
         const nextIterationStartMillis = iterationStart + this.workerCycleTimeMs;
-        const backoffTimeMillis = nextIterationStartMillis - performance.now();
+        const backoffTimeMillis = Math.round(nextIterationStartMillis - performance.now());
         if (backoffTimeMillis > 0) {
           this.workerBackoffTime += backoffTimeMillis;
-          await sleep(backoffTimeMillis);
+          if (backoffTimeMillis >= 1.0) {
+            await sleep(backoffTimeMillis);
+          }
+          while (performance.now() < nextIterationStartMillis) {
+            /* busy-wait if less than 1ms */
+          }
+          iterationStart = nextIterationStartMillis;
+        } else if (backoffTimeMillis == 0) {
           iterationStart = nextIterationStartMillis;
         } else {
-          // TODO: behave better for sub-millisecond backoff times
-
           this.workerBehindScheduleTime += -backoffTimeMillis;
           if (-backoffTimeMillis > this.workerCycleTimeMs) {
-            // Record missing iterations' latencies - we're pretending that the requests were sent and failed, see https://www.scylladb.com/2021/04/22/on-coordinated-omission/
+            // Record request latency for missed iterations. We're pretending that the requests were enqueued but failed. See: https://www.scylladb.com/2021/04/22/on-coordinated-omission/
+
+            // Mode 1: record a timeout for every missed iteration; this simulates a bounded queue with a fixed timeout
             for (
               let missingValueMs = iterationDurationMillis - this.workerCycleTimeMs;
-              missingValueMs >= this.workerCycleTimeMs;
+              missingValueMs > this.workerCycleTimeMs;
               missingValueMs -= this.workerCycleTimeMs
             ) {
-              // Choose how to track request latency for missed iterations (if at all); from least to most pessimistic.
-
-              // Mode 1: immediately fail requests that are behind schedule
-              // this.recordDuration(this.requestLatencyMicros, Math.round(missingValueMs * 1000));
-
-              // Mode 2: record the configured timeout value for any missed iterations
-              this.recordDuration(this.requestLatencyMicros, Math.round(this.timeoutValueMs * 1000));
-
-              // Mode 3: pretend the missing requests are waiting in the queue since their intended start time, and fail at the end of the test overall
-              // this.recordDuration(
-              //   this.requestLatencyMicros,
-              //   Math.round((endTime - iterationStart + missingValueMs) * 1000), // TODO: calculate queued time based on a fixed arrival rate
-              // );
-
+              this.recordDuration(
+                this.requestLatencyMicros,
+                missingValueMs > this.timeoutValueMs
+                  ? Math.round(this.timeoutValueMs * 1000)
+                  : Math.round(missingValueMs * 1000),
+              );
               this.missedIterations += 1;
             }
+
+            // Mode 2: pretend the requests have been waiting in the queue since their intended start time; simulates FIFO processing with an unbounded queue depth
+            // const requestLatencyMicros =
+            //   Math.round(iterationCompleted - (warmupEndTime + this.iterationCount * this.workerCycleTimeMs)) * 1000;
+            // this.recordDuration(this.requestLatencyMicros, requestLatencyMicros);
+            // const missedIterations = Math.ceil((iterationCompleted - iterationStart) / this.workerCycleTimeMs) - 1;
+            // this.missedIterations += missedIterations;
+
             iterationStart = iterationCompleted; // begin the next iteration immediately after the last request completed
           }
         }
-      } while (iterationStart < endTime);
+      } while (true);
     };
 
     for (let i = 0; i < this.concurrency; i++) {
@@ -205,7 +221,7 @@ export class LoadTestDriver {
         (this.errorIterations + this.missedIterations) / (this.iterationCount + this.missedIterations),
       iterationsPerSecondPerWorker: (this.iterationCount * 1_000) / this.concurrency / this.benchmarkDurationMs,
       workerCycleTimeMillis: this.workerCycleTimeMs,
-      requestsPerSecondOverall: (this.requestCount * 1_000) / this.benchmarkDurationMs,
+      throughputOverall: (this.requestCount * 1_000) / this.benchmarkDurationMs,
       targetArrivalRateRatio: (this.requestCount * 1_000) / this.benchmarkDurationMs / this.targetRps,
       requestLatencyStatsMillis: {
         avg: this.requestLatencyMicros.mean / 1_000,
